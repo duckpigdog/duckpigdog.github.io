4d2debd3940105deea5aeb9e1ee35ea0c8512125
duckpigdog <suc2es2.clc@gmail.com>
2026-04-23 01:25:07 +0800

    Site updated: 2026-04-23 01:25:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776751227123";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776878701200";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
